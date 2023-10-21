d044b0425a9231b0bdb1e0d4233af1665c5a8778
EthannLi <ethannli@hotmail.com>
2023-10-21 14:21:12 -0400

    ADDED more test cases

--- scripts/newscover/tests/test_newsapi.py
+++ scripts/newscover/tests/test_newsapi.py
@@ -2,25 +2,53 @@ import unittest
 import datetime
 from scripts.newscover.newsapi import fetch_latest_news
 
-class TestNewsAPI(unittest.TestCase):
-    def news_keyword_Is_None():
+class Test(unittest.TestCase):
+    def news_keyword_Is_None(self):
         api_key = "e7d7f14874b942fb9c02515cfe192063"
         news_keywords = ""
         with self.assertRaises(ValueError):
             fetch_latest_news(api_key, news_keywords)
 
-    def lookback_days(self):
+    def news_keyword(self):
+        api_key = "e7d7f14874b942fb9c02515cfe192063"
+        news_keywords = "jesus"
+        with self.assertRaises(ValueError):
+            fetch_latest_news(api_key, news_keywords)
+        
+class TestLookBack(unittest.TestCase):
+    def lookback_with_days(self):
         api_key = "e7d7f14874b942fb9c02515cfe192063"
         news_keywords = "tesla"
         lookback_days = 5
-        oldest_allowed_date = datetime.datetime.now() - datetime.timedelta(days=lookback_days)
+        oldest_allowed_date = datetime.datetime.now() - datetime.timedelta(days=int(lookback_days))
         news = fetch_latest_news(api_key, news_keywords, lookback_days)
         for article in news:
             article_date = datetime.strptime(article['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
             self.assertGreaterEqual(article_date, oldest_allowed_date)
-        
-    def news_keyword_nonAlpha():
+
+
+    def lookback_without_days(self):
+        api_key="e7d7f14874b942fb9c02515cfe192063"
+        news_keywords= "tesla"
+        oldest_allowed_date = datetime.datetime.now() - datetime.timedelta(days=int(lookback_days))
+        news = fetch_latest_news(api_key, news_keywords, lookback_days)
+        for article in news:
+            article_date = datetime.strptime(article['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
+            self.assertGreaterEqual(article_date, oldest_allowed_date)
+
+
+class TestNonAlpha(unittest.TestCase):        
+    def news_keyword_one_nonAlpha(self):
         api_key = "e7d7f14874b942fb9c02515cfe192063"
         news_keywords = "tesla@123"
         with self.assertRaises(ValueError):
             fetch_latest_news(api_key, news_keywords)
+
+    def news_keyword_all_nonAlpha(self):
+        api_key = "e7d7f14874b942fb9c02515cfe192063"
+        news_keywords = "@@@"
+        self.assertFail()
+        with self.assertRaises(ValueError):
+            fetch_latest_news(api_key, news_keywords)
+
+
