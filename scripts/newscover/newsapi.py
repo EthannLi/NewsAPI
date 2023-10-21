e09d9ba94db1d034001d7e237910978becad26bd
EthannLi <ethannli@hotmail.com>
2023-10-21 15:27:11 -0400

    FIX the tests cases, the newspi.py script by raising exceptions

--- scripts/newscover/newsapi.py
+++ scripts/newscover/newsapi.py
@@ -5,9 +5,8 @@ import datetime
 
 def fetch_latest_news(api_key,news_keywords,lookback_days=10):
     # Fails if news_keywords is empty or there are non-alphanumeric characters
-    if news_keywords == "" or not news_keywords.isalnum():
-        sys.error("news_keywords is empty")
-        return None
+    if news_keywords == "" or news_keywords is None or not news_keywords.isalnum():
+        raise ValueError("Invalid news keywords")
 
     if lookback_days is None:
         lookback_days = 10
@@ -17,10 +16,6 @@ def fetch_latest_news(api_key,news_keywords,lookback_days=10):
     dates = str(dates).split(" ")[0]
     response = requests.get("https://newsapi.org/v2/everything?q="+news_keywords+"&from="+dates+"&sortBy=publishedAt&apiKey="+api_key)
     # Fails if response is not 200 which means the request failed
-    if response.status_code != 200:
-        sys.error("Error fetching news from newsapi.org")
-        return None
     
     # Returns a list of english articles represented as dictionaries
     return json.loads(response.text)['articles']
-
--- scripts/newscover/tests/test_newsapi.py
+++ scripts/newscover/tests/test_newsapi.py
@@ -1,54 +1,52 @@
 import unittest
 import datetime
-from scripts.newscover.newsapi import fetch_latest_news
+from newscover.newsapi import fetch_latest_news
 
-class Test(unittest.TestCase):
-    def news_keyword_Is_None(self):
+class test_Keyword(unittest.TestCase):
+    def test_news_keyword_Is_Empty(self):
         api_key = "e7d7f14874b942fb9c02515cfe192063"
         news_keywords = ""
         with self.assertRaises(ValueError):
             fetch_latest_news(api_key, news_keywords)
 
-    def news_keyword(self):
+    def test_news_keyword_is_None(self):
         api_key = "e7d7f14874b942fb9c02515cfe192063"
-        news_keywords = "jesus"
+        news_keywords = None
         with self.assertRaises(ValueError):
-            fetch_latest_news(api_key, news_keywords)
+            fetch_latest_news(api_key,news_keywords)
         
-class TestLookBack(unittest.TestCase):
-    def lookback_with_days(self):
+class test_LookBack(unittest.TestCase):
+    def test_lookback_with_days(self):
         api_key = "e7d7f14874b942fb9c02515cfe192063"
         news_keywords = "tesla"
         lookback_days = 5
         oldest_allowed_date = datetime.datetime.now() - datetime.timedelta(days=int(lookback_days))
         news = fetch_latest_news(api_key, news_keywords, lookback_days)
         for article in news:
-            article_date = datetime.strptime(article['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
+            article_date = datetime.datetime.strptime(article['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
             self.assertGreaterEqual(article_date, oldest_allowed_date)
 
-
-    def lookback_without_days(self):
+    def test_lookback_without_days(self):
         api_key="e7d7f14874b942fb9c02515cfe192063"
         news_keywords= "tesla"
         oldest_allowed_date = datetime.datetime.now() - datetime.timedelta(10)
         news = fetch_latest_news(api_key, news_keywords)
         for article in news:
-            article_date = datetime.strptime(article['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
+            article_date = datetime.datetime.strptime(article['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
             self.assertGreaterEqual(article_date, oldest_allowed_date)
 
-
-class TestNonAlpha(unittest.TestCase):        
-    def news_keyword_one_nonAlpha(self):
+class test_NonAlpha(unittest.TestCase):        
+    def test_news_keyword_one_nonAlpha(self):
         api_key = "e7d7f14874b942fb9c02515cfe192063"
         news_keywords = "tesla@123"
         with self.assertRaises(ValueError):
             fetch_latest_news(api_key, news_keywords)
 
-    def news_keyword_all_nonAlpha(self):
+    def test_news_keyword_all_nonAlpha(self):
         api_key = "e7d7f14874b942fb9c02515cfe192063"
         news_keywords = "@@@"
-        self.assertFail()
         with self.assertRaises(ValueError):
             fetch_latest_news(api_key, news_keywords)
 
-
+if __name__ == "__main__":
+    unittest.main()
