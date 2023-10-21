a817222ad014ff131e54ec8a7c3fabb830401e61
EthannLi <ethannli@hotmail.com>
2023-10-21 11:10:10 -0400

    ADDED some test cases to make sure the script works

--- scripts/newscover/newscover.py
+++ scripts/newscover/newscover.py
@@ -3,25 +3,20 @@ import requests
 import sys
 import datetime
 
-def fetch_latest_news(api_key,news_keywords,lookback_days):
-    # Fails if news_keywords is empty
-    if news_keywords == "":
+def fetch_latest_news(api_key,news_keywords,lookback_days=10):
+    # Fails if news_keywords is empty or there are non-alphanumeric characters
+    if news_keywords == "" or not news_keywords.isalnum():
         sys.error("news_keywords is empty")
         return None
-    
+
     dates = datetime.datetime.now() - datetime.timedelta(days=lookback_days)
     # Just want the date part
     dates = str(dates).split(" ")[0]
-    print(dates)
-    # response = requests.get("https://newsapi.org/v2/everything?q="+news_keywords+"&from=2019-04-01&sortBy=publishedAt&apiKey="+api_key)
-    # if response.status_code != 200:
-    #     sys.error("Error fetching news from newsapi.org")
-    #     return None
-    #return response.json()
-
-def main():
-    fetch_latest_news("e7d7f14874b942fb9c02515cfe192063","bitcoin",7)
+    response = requests.get("https://newsapi.org/v2/everything?q="+news_keywords+"&from="+dates+"&sortBy=publishedAt&apiKey="+api_key)
+    # Fails if response is not 200 which means the request failed
+    if response.status_code != 200:
+        sys.error("Error fetching news from newsapi.org")
+        return None
     
 
-if __name__ == "__main__":
-    main()
+    return response.json()
---
+++ scripts/newscover/tests/test_newsapi.py
@@ -0,0 +1,26 @@
+import unittest
+import datetime
+from newscover.newscover import fetch_latest_news
+
+class TestNewsAPI(unittest.TestCase):
+    def news_keyword_Is_None():
+        api_key = "e7d7f14874b942fb9c02515cfe192063"
+        news_keywords = ""
+        with self.assertRaises(ValueError):
+            fetch_latest_news(api_key, news_keywords)
+
+    def lookback_days(self):
+        api_key = "e7d7f14874b942fb9c02515cfe192063"
+        news_keywords = "tesla"
+        lookback_days = 5
+        oldest_allowed_date = datetime.datetime.now() - datetime.timedelta(days=lookback_days)
+        news = fetch_latest_news(api_key, news_keywords, lookback_days)
+        for article in news:
+            article_date = datetime.strptime(article['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
+            self.assertGreaterEqual(article_date, oldest_allowed_date)
+        
+    def news_keyword_nonAlpha():
+        api_key = "e7d7f14874b942fb9c02515cfe192063"
+        news_keywords = "tesla@123"
+        with self.assertRaises(ValueError):
+            fetch_latest_news(api_key, news_keywords)
